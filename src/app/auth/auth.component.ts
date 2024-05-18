e4201def89d69af4142dc6995dde01ad74c59905
LucasBegnini10 <begninilucas12@gmail.com>
2024-05-17 22:19:42 -0300

    feat: create form login and add validations

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,58 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-auth',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
 })
 export class AuthComponent {
   constructor() {}
 
-  optionsAuth = [
-    {id: 'apple', name: 'Apple', image: 'assets/logos/apple.png'},
-    {id: 'google', name: 'Google', image: 'assets/logos/google.png'},
-    {id: 'github', name: 'Github', image: 'assets/logos/github.png'},
-  ]
+  MIN_LENGTH_PASSWORD=6;
 
+  optionsAuth = [
+    { id: 'apple', name: 'Apple', image: 'assets/logos/apple.png' },
+    { id: 'google', name: 'Google', image: 'assets/logos/google.png' },
+    { id: 'github', name: 'Github', image: 'assets/logos/github.png' },
+  ];
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    pwd: new FormControl('', [Validators.required, Validators.minLength(this.MIN_LENGTH_PASSWORD)]),
+    remember: new FormControl(false)
+  });
+
+  get email(){
+    return this.loginForm.get('email')
+  }
+
+  get password(){
+    return this.loginForm.get('pwd')
+  }
+
+  getEmailIsEmpty() {
+    return this.email?.hasError('required');
+  }
+  getEmailIsInvalid() {
+    return this.email?.hasError('email');
+  }
+
+  getPasswordIsEmpty() {
+    return this.password?.hasError('required');
+  }
+
+  getPasswordMinLengthError() {
+    return this.password?.hasError('minlength');
+  }
+
+  onSubmit(){
+    if(this.loginForm.status === "INVALID") return;
+
+    console.log(this.loginForm.value)
+
+
+  }
 }
