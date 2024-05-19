804966bf5a79cb52a59fed7e91ef271b44dbad7f
LucasBegnini10 <begninilucas12@gmail.com>
2024-05-19 14:15:37 -0300

    fix: change public api

--- src/app/pages/shortener/service/shortener.service.ts
+++ src/app/pages/shortener/service/shortener.service.ts
@@ -5,14 +5,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ShortenerService {
-  private url = 'https://ulvis.net/API/write/get';
+  private url = 'https://cleanuri.com/api/v1/shorten';
 
   constructor(private httpClient: HttpClient) {}
 
   shortenUrl(url: string) {
-    return this.httpClient.get(`${this.url}`, {
-      params: { url: this.formatUrl(url) },
-    });
+    return this.httpClient.post(`${this.url}`, { url: this.formatUrl(url) });
   }
 
   private formatUrl(url: string) {
